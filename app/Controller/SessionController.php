ba2ad98fb7f582c17b915d8544c331a01af7d953
nakajima-satoru <nakajima@email.com>
2023-03-27 18:32:05 +0900

    3/27

--- app/Controller/SessionController.php
+++ app/Controller/SessionController.php
@@ -2,11 +2,33 @@
 
 namespace App\Controller;
 
+use Reald\FormUi\FormUi;
+use Reald\Session\Session;
+
 class SessionController extends Controller{
 
     public function index(){
 
+        $session = new Session;
+
+        $form = new FormUi;
+
+        $this->Response->setData([
+            "form" => $form,
+            "session" => $session,
+        ]);
         
+        if($this->Request->post()->exists()){
+
+            $post = $this->Request->post()->get();
+
+
+            $session->write("value_1", $post["value_1"]);
+
+            $this->Response->redirect();            
+        }
+
+
     }
 
 }
